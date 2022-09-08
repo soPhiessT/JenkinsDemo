7587ed8ef87e1b9a537c1a5b0b704e03523a7202
soPhiessT <sopio.sturua.1@iliauni.edu.ge>
2022-09-08 19:40:21 +0400

    for jenkins

---
+++ src/test/java/JenkinsDemo.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+    @Test
+    public void homework(){
+        int a = 5;
+        int b = 10;
+        System.out.println(a + b);
+    }
+}
